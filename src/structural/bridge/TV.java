ab5640bb169366783935edb64b137e6aa70d02c2
Anurag Ambuj <anuragambuj000@gmail.com>
2016-06-19 16:00:47 +0530

    Bridge pattern example under Strucural Pattern added

---
+++ src/structural/bridge/TV.java
@@ -0,0 +1,10 @@
+package structural.bridge;
+
+/**
+ * Created by anurag on 19/6/16.
+ */
+public interface TV {
+    public void powerOn();
+    public void powerOff();
+    public void changeChannel(int channelNumber);
+}
